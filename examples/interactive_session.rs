3613ad0f20704a51583d1f2b887b557d532a3b9f
Angel Leon <gubatron@gmail.com>
2023-10-12 20:02:50 -0600

    use gpt-4 model

--- examples/interactive_session.rs
+++ examples/interactive_session.rs
@@ -18,7 +18,7 @@ async fn main() {
         .expect("Please set the OPEN_AI_SECRET environment variable!");
 
     // Instantiate the OpenAI client
-    let client = OpenAIClient::new(&secret_key, "gpt-4-0613");
+    let client = OpenAIClient::new(&secret_key, "gpt-4");
 
     // Set up the LLMSession
     let system_prompt = "You are an award winning bitcoin/blockchain/crypto/tech/software journalist for DiarioBitcoin, you are spanish/english bilingual, you can write in spanish at a professional journalist level, as well as a software engineer. You are hold a doctorate in economy and cryptography. When you answer you don't make any mentions of your credentials unless specifically asked about them.".to_string();
